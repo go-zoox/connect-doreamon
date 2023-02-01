6faec63a81ce427fc401c01b464148ff37d16c3f
whatwewant <tobewhatwewant@outlook.com>
2023-02-02 03:08:51 +0800

    fix: upstream or frontend+backend

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"net/url"
+	"strings"
 
+	"github.com/go-zoox/core-utils/cast"
 	"github.com/go-zoox/core-utils/fmt"
+	"github.com/go-zoox/core-utils/regexp"
 	"github.com/go-zoox/random"
 
 	"github.com/go-zoox/cli"
@@ -57,6 +61,21 @@ func main() {
 				Usage:   "Doreamon Client Secret",
 				EnvVars: []string{"REDIRECT_URI"},
 			},
+			&cli.StringFlag{
+				Name:    "frontend",
+				Usage:   "frontend service",
+				EnvVars: []string{"FRONTEND"},
+			},
+			&cli.StringFlag{
+				Name:    "backend",
+				Usage:   "backend service",
+				EnvVars: []string{"BACKEND"},
+			},
+			&cli.StringFlag{
+				Name:    "upstream",
+				Usage:   "upstream service",
+				EnvVars: []string{"UPSTREAM"},
+			},
 			&cli.BoolFlag{
 				Name:    "debug",
 				Usage:   "Debug mode show config info",
@@ -74,6 +93,9 @@ func main() {
 		clientID := c.String("client-id")
 		clientSecret := c.String("client-secret")
 		redirectURI := c.String("redirect-uri")
+		frontend := c.String("frontend")
+		backend := c.String("backend")
+		upstream := c.String("upstream")
 		debug := c.Bool("debug")
 
 		var cfg *config.Config
@@ -122,6 +144,61 @@ func main() {
 				},
 			}
 		}
+		if upstream != "" {
+			if regexp.Match(":", upstream) {
+				u, err := url.Parse(upstream)
+				if err != nil {
+					return fmt.Errorf("upstream format error, protocol://host:port")
+				}
+
+				cfg.Upstream = config.ConfigPartService{
+					Protocol: u.Scheme,
+					Host:     u.Hostname(),
+					Port:     cast.ToInt64(u.Port()),
+				}
+			} else {
+				parts := strings.Split(upstream, ":")
+				if len(parts) != 2 {
+					return fmt.Errorf("upstream format error, host:port")
+				}
+
+				cfg.Upstream = config.ConfigPartService{
+					Host: parts[0],
+					Port: cast.ToInt64(parts[1]),
+				}
+			}
+		} else {
+			if frontend == "" || backend == "" {
+				return fmt.Errorf("frontend and backend are required")
+			}
+
+			{
+				u, err := url.Parse(frontend)
+				if err != nil {
+					return fmt.Errorf("frontend format error, protocol://host:port")
+				}
+
+				cfg.Frontend = config.ConfigPartService{
+					Protocol: u.Scheme,
+					Host:     u.Hostname(),
+					Port:     cast.ToInt64(u.Port()),
+				}
+			}
+
+			{
+				u, err := url.Parse(backend)
+				if err != nil {
+					return fmt.Errorf("backend format error, protocol://host:port")
+				}
+
+				cfg.Backend = config.ConfigPartService{
+					Protocol: u.Scheme,
+					Host:     u.Hostname(),
+					Port:     cast.ToInt64(u.Port()),
+				}
+			}
+		}
+
 		if cfg.SecretKey == "" {
 			cfg.SecretKey = random.String(10)
 		}
